aeed5f812f9d1c4a1ffe57a43df16bdf027a26a7
livan <98717705+Livanjoel@users.noreply.github.com>
2022-11-28 05:35:38 -0500

    update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
-USERBOT_SESSION = os.environ.get("USERBOT_SESSION", None)
+USERBOT_SESSION = os.environ.get("USERBOT_SESSION")
 
 Client(USERBOT_SESSION, api_hash=API_HASH,api_id=API_ID).run()
 
